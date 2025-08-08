1e0e9aee366929626dc6299d4e77caaedf3e4445
nfridoy <nfridoy@gmail.com>
2025-08-08 17:35:58 +0600

    init Msg: No messages yet. Start a new conversation!

--- src/components/shared/DashboardNavbar.jsx
+++ src/components/shared/DashboardNavbar.jsx
@@ -1,11 +1,18 @@
 import { NavLink } from "react-router";
+import logo from './../../assets/logo.png'
 
 export default function DashboardNavbar() {
     return (
         <div className="flex flex-row items-center justify-between py-4 px-2 bg-gray-100 ">
             <div>
                 <NavLink to={"/"} className='text-2xl'>
-                    Gameplan
+                    {/* Gameplan */}
+                    <span className="flex flex-row items-center justify-start space-x-0.5">
+                                            <img src={logo} alt="" srcset="" className="size-10"/> 
+                                            <span>
+                                                gameplan
+                                            </span>
+                                        </span>
                 </NavLink>
             </div>
             <ol className="flex flex-row gap-5">
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,11 +1,18 @@
 import { NavLink } from "react-router";
+import logo from './../../assets/logo.png'
 
 export default function Navbar() {
     return (
         <div className="flex flex-row justify-between mx-10 my-10">
             <div>
                 <NavLink to={"/"} className='text-4xl' >
-                    Gameplan
+                    {/* Gameplan */}
+                    <span className="flex flex-row items-center justify-start space-x-0.5">
+                        <img src={logo} alt="" srcset="" className="size-10"/> 
+                        <span>
+                            gameplan
+                        </span>
+                    </span>
                 </NavLink>
             </div>
             <ol className="flex flex-row gap-5">
